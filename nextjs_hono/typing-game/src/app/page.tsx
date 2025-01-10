f22391aaeba36e80094dc347a83a5e92f4cec688
toketime <flatoke@gmail.com>
2025-01-10 20:48:00 +0900

    hono

--- nextjs_hono/typing-game/src/app/page.tsx
+++ nextjs_hono/typing-game/src/app/page.tsx
@@ -1,101 +1,142 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const questions = [
+    { question: "React", image: "/monster1.jpg" },
+    { question: "TypeScript", image: "/monster2.jpg" },
+    { question: "JISOU", image: "/monster3.jpg" },
+    { question: "GitHub", image: "/monster4.jpg" },
+    { question: "Next.js", image: "/monster5.jpg" },
+  ];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [currentPosition, setCurrentPosition] = useState(0);
+  const [isCompleted, setIsCompleted] = useState(false);
+  const [isStarted, setIsStarted] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [startTime, setStartTime] = useState(0);
+  const [totalTime, setTotalTime] = useState(0);
+  const [score, setScore] = useState(0);
+
+  const addResult = (userName: string, startTime: number) => {
+    const endTime = Date.now();
+    const totalTime = endTime - startTime;
+    const timeInSeconds = totalTime /1000;
+    const baseScore = 10000;
+    const timeDeduction = Math.floor(timeInSeconds * 100);
+    const score = Math.max(1000, baseScore - timeDeduction);
+
+    return { totalTime, score };
+  };
+
+  useEffect(() => {
+    const handleKeyDown = async (e: KeyboardEvent) => {
+      const currentQuestion = questions[currentQuestionIndex];
+      if (
+        e.key.toLowerCase() ===
+        currentQuestion.question[currentPosition].toLowerCase()
+      ) {
+        setCurrentPosition((prev) => prev + 1);
+      }
+
+      if (currentPosition === currentQuestion.question.length - 1) {
+        if (currentQuestionIndex === questions.length - 1) {
+          const { totalTime, score } = addResult(userName, startTime);
+          setTotalTime(totalTime);
+          setScore(score);
+        
+          setIsCompleted(true);
+        } else {
+          setCurrentQuestionIndex((prev) => prev + 1);
+          setCurrentPosition(0);
+        }
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [currentPosition, currentQuestionIndex]);
+
+  const handleStart = () => {
+    if (!userName) {
+      alert("名前を入力してください");;
+      return;
+    }
+
+    setIsStarted(true);
+
+    setStartTime(Date.now());
+  };
+
+  if (!isStarted) {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-center bg-black">
+        <div className="text-center p-8">
+          <input 
+            type="text"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+            placeholder="Enter your name..."
+            className="w-64 p-3 text-lg"
+          />
+        </div>
+        <div>
+          <button 
+          onClick={handleStart}
+          className="px-8 py-3 text-xl bg-red-900"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            Start Game!!!!
+          </button>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    );
+  }
+
+  if (isCompleted) {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
+        <div className="text-center p-8">
+          <h2>Result</h2>
+          <div className="mb-8 space-y-2">
+            <p>Player: {userName}</p>
+            <p>
+              Time
+              <span>{(totalTime / 1000).toFixed(2)}</span>
+              seconds
+            </p>
+            <p>Score: {score}</p>
+          </div>
+        </div>
+      </main>
+    );
+  }
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div
+        className="text-center w-full h-screen bg-cover bg-center flex flex-col items-center justify-center"
+        style={{
+          backgroundImage: `url(${questions[currentQuestionIndex].image})`,
+          backgroundColor: "rgba(0, 0, 0, 0.7)",
+          backgroundBlendMode: "overlay",
+        }}
+      >
+        <div>
+          {questions[currentQuestionIndex].question
+            .split("")
+            .map((char, index) => (
+              <span
+                key={index}
+                style={{
+                  color: index < currentPosition ? "#ff0000" : "white",
+                }}
+              >
+                {char}
+              </span>
+            ))}
+        </div>
+      </div>
+    </main>
   );
 }
